b9cb68fff688791f40b3b141631aa43873b171ce
Bin Tran <sudocode.bin@gmail.com>
2017-02-26 12:06:10 -0800

    Last time

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,9 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: {minimum: 5}
   validates :body, presence: true, uniqueness: true, length: {minimum: 10}
   # setting default url for img_url
-  def img_url(url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7G9JTqB8z1AVU-Lq7xLy1fQ3RMO-Tt6PRplyhaw75XCAnYvAYxg")
-    self[:img_url] || url
-  end
+  # def img_url(url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7G9JTqB8z1AVU-Lq7xLy1fQ3RMO-Tt6PRplyhaw75XCAnYvAYxg")
+  #   self[:img_url] || url
+  # end
 
   # add all vote amount to get sum of votes for post
   def score
